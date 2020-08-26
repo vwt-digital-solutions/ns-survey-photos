2991491a59a7a15f3c57df671dba6628d54c811d
nicogriffioen <60692751+nicogriffioen@users.noreply.github.com>
2020-08-26 14:35:51 +0200

    Insert changes from config repository. DAT-4047
    There are two repositories where the NS-surveys photo function exists, this one, and the internal config repository for ns-surveysingest, both of which contain some changes. This repository is the most logical one for code, so this commit merges the changes from both repositories.
    Git stats

--- functions/download_survey_photos/main.py
+++ functions/download_survey_photos/main.py
@@ -2,11 +2,14 @@ import os
 import logging
 import json
 import base64
+import tempfile
 import requests
+
 import config
 
-from google.cloud import storage, kms_v1
+from google.cloud import storage, kms_v1, secretmanager_v1
 from google.api_core.exceptions import ServiceUnavailable
+
 from requests_oauthlib import OAuth1
 from retry import retry
 
@@ -22,39 +25,58 @@ def get_authentication_secret():
     return decrypt_response.plaintext.decode("utf-8").replace('\n', '')
 
 
+def get_secret():
+    secret_manager_client = secretmanager_v1.SecretManagerServiceClient()
+
+    secret_name = secret_manager_client.secret_version_path(
+        os.environ['PROJECT_ID'],
+        os.environ['SECRET_NAME'],
+        'latest')
+
+    response = secret_manager_client.access_secret_version(secret_name)
+    payload = response.payload.data.decode('UTF-8')
+
+    return payload
+
+
 def get_data_from_store(bucket_name, source):
     bucket = client.get_bucket(bucket_name)
     blob = bucket.get_blob(source)
     content = blob.download_as_string().decode("utf-8")
     try:
         data = json.loads(content)
-        if data['elements']:
+        if data.get('elements', []):
             return {
                 'survey': data['elements'][0]['info']['formId'], 'registrations': [{
                     'registration': single['meta']['serialNumber'],
                     'attachments': parse_survey(single['data'], [])
                 } for single in data['elements']]
             }
-    except Exception as e:
-        logging.error(f"Failure processing survey, skip. Reason {e}")
+    except Exception:
+        logging.exception(f"Failure processing survey {source}, skipped.")
 
 
 @retry(ServiceUnavailable, tries=3, delay=2)
-def download_photo_if_absent(form, registration, photos):
+def download_photo_if_absent(form, registration, images):
     bucket = client.get_bucket(config.GOOGLE_STORAGE_BUCKET)
-    for photo in photos:
-        photo_name = f"{config.PHOTO_PATH}/{form}/{registration}/{photo}"
-        blob = bucket.blob(photo_name)
-        if blob.exists():
-            logging.info(f"photo {photo_name} already downloaded, skip")
+    for image in images:
+        photo_name = f"{config.PHOTO_PATH}/{form}/{registration}/{image}"
+        blob = storage.Blob(bucket=bucket, name=photo_name)
+        if blob.exists(client):
+            logging.info(f"image {photo_name} already downloaded, skip")
         else:
-            store_photo(blob, photo)
+            store_photo(blob, image)
     pass
 
 
-def store_photo(blob, photo):
-    logging.info(f"downloading photo {photo}")
-    consumer_secret = get_authentication_secret()
+def store_photo(blob, image):
+    """
+    Store foto as a png
+    :param blob:
+    :param image:
+    """
+    logging.info(f"downloading image {image}")
+    consumer_secret = get_secret()
     consumer_key = config.CONSUMER_KEY
     oauth_1 = OAuth1(
         consumer_key,
@@ -63,30 +85,37 @@ def store_photo(blob, photo):
     )
 
     data_response = requests.get(
-        f"{config.MORE_APP_DOWNLOAD_URL_PREFIX}{photo}{config.MORE_APP_DOWNLOAD_URL_SUFFIX}",
+        f"{config.MORE_APP_DOWNLOAD_URL_PREFIX}{image}{config.MORE_APP_DOWNLOAD_URL_SUFFIX}",
         auth=oauth_1,
         headers={}
     )
 
-    b64_data = base64.b64encode(data_response.content)
-    blob.upload_from_string(data=b64_data, content_type="text/plain")
+    content_type = data_response.headers['content-type'].split(';')[0]
+    image_format = content_type.split('/')[1]
+
+    registration_image = f'{tempfile.gettempdir()}/{image}.{image_format}'
+    image = open(registration_image, 'w+b')
+    image.write(bytearray(data_response.content))
+    image.close()
+    blob.upload_from_filename(registration_image, content_type=content_type)
 
 
-def parse_survey(content, photos):
+def parse_survey(content, images):
     if type(content).__name__ == 'list':
         for element in content:
-            parse_survey(element, photos)
+            parse_survey(element, images)
     elif type(content).__name__ == 'dict':
         for element in content:
-            parse_survey(content[element], photos)
+            parse_survey(content[element], images)
     elif type(content).__name__ == 'str' and content.startswith("gridfs://registrationFiles/"):
-        photos.append(content.split('/')[-1])
-    return photos
+        images.append(content.split('/')[-1])
+    return images
 
 
 def process_survey_attachments(data, context):
     bucket = data['bucket']
     source = data['name']
+    logging.info(f'New Blob: {source}')
 
     if source.startswith('source'):
         refs = get_data_from_store(bucket, source)
